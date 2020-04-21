c407d555cd0326dbd55b5769fc9841765f39e4a2
Orionic <daniil.shadow@mail.ru>
2020-04-21 21:50:37 +0300

    lab-2.0

--- index.php
+++ index.php
@@ -1,31 +1,72 @@
 <?php
 
-	/**
-	 * 
-	 */
-	class B 
-	{
-	}
+class KKException extends RuntimeException
+{
+}
+/**
+ *
+ */
+class B
+{
+    protected $x;
+    function solve($a,$b,$c)
+    {
+        try
+        {
+            if ($a==0)
+            {
+                throw new KKException("Equation does not exist", 1);
+            }
+            else
+            {
+                $x=(-1*$b)/$a;
+            }
+        } catch (KKException $e) {
+            $x=$e;
+        }
+        $this->x=$x;
+        return $x;
+    }
+}
 
-	/**
-	 * 
-	 */
-	class A extends B
-	{
-		public $a1;
-        public $a2;
+/**
+ *
+ */
+class A extends B
+{
+    protected function discr($a,$b,$c)
+    {
+        $discr=pow($b,2)-4*$a*$c;
+        try {
+            if ($discr<0)
+            {
+                throw new KKException("Discriminant less than zero", 1);
+            }
+        } catch (KKException $e) {
+            $discr=$e;
+        }
+        return $discr;
+    }
 
-		function __construct($a,$b)
-		{
-			$this->a1 = $a;
-			$this->a2 = $b;
-		}
-	}
-
-	$b1=new B();
-	$b2=new B();
-	$b3=new B();
-
-	$a2=new A($b2,$b3);
-	$a1=new A($b1,$a2);
-?>
+    function solve($a,$b,$c)
+    {
+        $discr=$this->discr($a,$b,$c);
+        if (is_object($discr)==true){
+            return $discr;
+        }
+        else if ($a==0) {
+            $x[]=parent::solve($b,$c,$a);
+        }
+        else if ($discr == 0)
+        {
+            $x[]=($b*-1)/(2*$a);
+        }
+        else
+        {
+            $x[]=(($b*-1)+sqrt($discr))/(2*$a);
+            $x[]=(($b*-1)-sqrt($discr))/(2*$a);
+        }
+        $this->x=$x;
+        return $x;
+    }
+}
